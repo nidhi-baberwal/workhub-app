7c82fb8edf38515e2d3e9d45bd2cced39d11608d
nidhi-baberwal <nidhibagoria29@gmail.com>
2026-06-03 22:45:07 +0530

    Add health check route

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 
+//Log every request
 app.use((req, res, next) => {
   console.log("HIT:", req.method, req.url);
   next();
@@ -15,6 +16,11 @@ app.use((req, res, next) => {
 //middleware
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("TaskFlow API is running");
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/workspaces", workspaceRoutes);
